fde8027741c29ac6620eaf2f7851c5a17254dc17
Alison <zhangyinag@126.com>
2019-07-10 10:23:10 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,14 @@
   <div class="home">
     <div class="text-right">
       <div class="m-2" v-if="$auth.principle">
-        <span class="mr-2">{{$auth.username}}</span>
-        <a @click="onLogout">登出</a>
+        <div>
+          <a @click="$router.push('/')">首页（Auth Only）</a>
+          <a @click="$router.push('/dashboard')" class="ml-2">仪表盘（Admin Only）</a>
+        </div>
+        <div>
+          <span class="mr-2">{{$auth.username}}</span>
+          <a @click="onLogout">登出</a>
+        </div>
       </div>
     </div>
     <router-view/>
@@ -16,9 +22,7 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class Home extends Vue {
   onLogout () {
-    this.$auth.logout().then(() => {
-      this.$router.push('/login')
-    })
+    this.$auth.logout()
   }
 }
 </script>
